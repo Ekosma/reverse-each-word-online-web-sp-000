2902a3f59bd5d282a8c95e91fc1d392907c78191
Evelyn Kosma <ezranaja@gmail.com>
2021-02-23 23:42:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(array)
-  asplit = array.split(" ")
-  backwards = asplit.each {|x| x.reverse!}
+  backwards = array.split(" ")
+  backwards.collect {|x| x.reverse!}
   return backwards.join(" ")
 end
